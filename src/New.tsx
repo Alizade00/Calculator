1ab67782bd612aedba53b2dfde146e4d31ced87e
Alizade00 <turkanlizad430@gmail.com>
2023-10-31 15:20:51 +0400

    init

---
+++ src/New.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function New(){
+    return(
+        <SafeAreaView>
+            <Text>
+                Turkan
+            </Text>
+        </SafeAreaView>
+    )
+}
+export default New
