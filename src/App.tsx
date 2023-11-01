4d3dfe44e9fb6049a04c47715bb435d591266860
Alizade00 <turkanlizad430@gmail.com>
2023-11-01 15:02:18 +0400

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,47 @@
-import React from 'react';
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, Pressable, SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-function New() {
+function App() {
   return (
-    <SafeAreaView style={styles.area}>
-      <View style={styles.header}>
-        <Image style={{ marginLeft: 30, marginTop: 68 }} source={require('./assets/icons/Back.png')} />
-        <View>
-          <Image style={styles.plant} source={require('./assets/images/plant.png')} />
-        </View>
-        <Image style={{ marginRight: 20, marginTop: 52 }} source={require('./assets/icons/Like.png')} />
-      </View>
-      <View style={styles.text}>
-        <Text style={{ marginLeft: 30, fontSize: 24, fontWeight: '800', color: 'black' }}>Astrophytum</Text>
-        <Text style={{ marginRight: 30, fontSize: 20, fontWeight: '800', color: 'black' }}>$312.00</Text>
-        <View>
-          <Image style={{ marginTop: 10 }} source={require('./assets/icons/Selection.png')} />
-        </View>
-      </View>
-      <View style={styles.pot}>
-        <Text>SELECT POT</Text>
-      </View>
-
-
+    <SafeAreaView style={styles.main}>
+      <Text style={styles.text}>0</Text>
+      <Pressable style={({pressed}) =>({
+        ...styles.button, 
+        backgroundColor: pressed ? 'tomato' : 'black',
+        opacity: pressed ? 0.5 : 1,
+        height: pressed ? 60 : 40})}>
+      <Text style={styles.buttonlabel}>Click me</Text>
+      </Pressable>
+      <Button title="Increment" />
+      <Button title="Deccrement" />
     </SafeAreaView>
   )
 }
-export default New
+export default App
 
-const styles = StyleSheet.create({
-  area: {
-    flex: 1,
-    backgroundColor: '#F1F4FB',
+const styles=StyleSheet.create ({
+  main: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'white'
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  text:{
+    fontSize:50,
+    fontWeight:'bold',
+    color:'aqua',
   },
-  plant: {
-    marginTop: 37,
+  button:{
+    backgroundColor:'black',
+    borderRadius: 100,
+    width: '80%',
+    height: 40,
+    justifyContent:'center',
+    alignItems:'center'
   },
-  text: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'baseline',
-    height: 100,
-    backgroundColor: 'white',
-    borderRadius: 50,
-    marginHorizontal: 8,
-    paddingTop: 46,
-  },
-  pot: {
-    backgroundColor:'white',
-    fontWeight:'800'
+  buttonlabel: {
+    color:'white',
+    textTransform:'uppercase',
+    fontWeight:'600'
   }
 })
