08b058d39f0bd7e853f5e4803e3e2e93de8b7e83
Alizade00 <turkanlizad430@gmail.com>
2023-11-16 08:39:51 +0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,18 @@
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+import { Home } from "./views/Home";
+import { Details } from "./views/Details";
+import { NavigationContainer } from "@react-navigation/native";
 
-function App() {
-  return (
-    <SafeAreaView style={styles.area}>
-      <View style={styles.header}>
-        <Image source={require('./assets/icons/Menu.png')} />
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Image style={{ marginRight: 46 }} source={require('./assets/icons/Basket.png')} />
-          <Image source={require('./assets/icons/Search.png')} />
-        </View>
-      </View>
-      <View style={styles.menu}>
-        <Text style={styles.menuText}>Concept</Text>
-        <Text style={{ ...styles.menuText, color: '#D2D2D2' }}>Popular</Text>
-        <Text style={{ ...styles.menuText, color: '#D2D2D2' }}>New</Text>
-      </View>
-      <View style={styles.rectangle}>
-        <Image source={require('./assets/icons/Rectangle.png')} />
-      </View>
-      <View style={styles.filter}>
-        <Image source={require('./assets/icons/Filter.png')} />
-      </View>
-      <ScrollView horizontal={true}>
-        <View style={styles.plant}>
-          <Text style={styles.plantText}>Gasteria Kyoryu</Text>
-          <Text style={{ paddingLeft: 33, fontSize: 9 }}>w 300 × h 310 mm</Text>
-          <Image source={require('./assets/images/Plant1.png')} />
-          <View style={styles.footer}>
-            <Text style={{color: 'black', fontSize: 20, fontWeight: '800', marginLeft: 20, marginTop:-40}}>$228.00</Text>
-            <View style={styles.button}>
-              <Text style={{color:'white', fontSize:30}}>+</Text>
-            </View>
-          </View>
-        </View>
-        <View style={styles.plant2}>
-        <Text style={styles.plantText2}>Astrophytum</Text>
-        <Text style={{ paddingLeft: 33, fontSize: 7 }}>w 230 × h 310 mm</Text>
-        <Image style={{marginTop:25, marginLeft:15}} source={require('./assets/images/Plant2.png')}/>  
-        <Text style={{color: 'black', fontSize: 15, fontWeight: '700', marginLeft: 24, marginTop:40}}>$242.00</Text>
-        </View>
-      </ScrollView>
-      <View style={styles.slider}>
-        <Image source={require('./assets/icons/Slider.png')}/>
-      </View>
-    </SafeAreaView>
+const Stack= createStackNavigator()
+export const App=() => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='Details' component={Details}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
-export default App
-
-const styles = StyleSheet.create({
-  area: {
-    flex: 1,
-  },
-  header: {
-    flexDirection: 'row',
-    marginHorizontal: 29,
-    marginTop: 29,
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  menu: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    marginTop: 83,
-    marginLeft: 26,
-    marginRight: 65,
-  },
-  menuText: {
-    fontSize: 17,
-    fontWeight: '800',
-    color: 'black'
-  },
-  rectangle: {
-    marginLeft: 28,
-    marginTop: 5,
-  },
-  filter: {
-    marginTop: 47,
-    alignItems: 'flex-end',
-    marginHorizontal: 27,
-  },
-  plant: {
-    backgroundColor: '#F1F4FB',
-    borderRadius: 24,
-    paddingTop: 12,
-    height: 450, 
-  },
-  plantText: {
-    fontSize: 21,
-    color: 'black',
-    paddingLeft: 31,
-    paddingTop: 35,
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  button: {
-      backgroundColor: '#1DA154',
-      width: 70,
-      height: 75,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderBottomRightRadius: 24,
-      borderTopLeftRadius: 12,
-      marginTop:-70,
-  },
-  slider: {
-    marginBottom:15, 
-    marginLeft: 12
-  },
-  plant2: {
-    backgroundColor: '#F1F4FB',
-    borderRadius: 24,
-    marginLeft:40,
-    marginTop:40,
-    height: 380,
-
-  },
-  plantText2 : {
-    fontSize: 16,
-    color: 'black',
-    paddingLeft: 31,
-    paddingTop: 35,
-    fontWeight: '400'
-  }
-
-})
---
+++ src/views/Details.tsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export const DetailsView =() => {
+  const [count, setCount] = useState(1);
+
+  const plus =() => {
+    setCount(count + 1);
+  };
+  const minus =() => {
+      count !== 0 && setCount(count - 1)
+  };
+
+
+  return (
+    <SafeAreaView style={styles.area}>
+      <View style={styles.header}>
+        <Image style={{ marginLeft: 30, marginTop: 68 }} source={require('./assets/icons/Back.png')} />
+        <View>
+          <Image style={styles.plant} source={require('./assets/images/plant.png')} />
+        </View>
+        <Image style={{ marginRight: 20, marginTop: 52 }} source={require('./assets/icons/Like.png')} />
+      </View>
+      <View style={styles.text}>
+        <View>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Text style={{ marginLeft: 30, fontSize: 24, fontWeight: '800', color: 'black' }}>Astrophytum</Text>
+            <Text style={{ fontSize: 20, fontWeight: '800', color: 'black' }}>$312.00</Text>
+          </View>
+          <View style={styles.selectButton}>
+            <TouchableOpacity style={{flexDirection:'row'}} onPress={minus}>
+              <Text style={{ fontSize: 25, color: 'black', fontWeight:'300' }}>-</Text>
+            </TouchableOpacity>
+            <Text style={{ fontSize:20, fontWeight: '800', color: 'black' }}>{count}</Text>
+            <TouchableOpacity onPress={plus}>
+              <Text style={{ fontSize: 25, color: 'black', fontWeight:'300' }}>+</Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={styles.selectPot}>SELECT POT</Text>
+            <View style={styles.pot}>
+              <Image source={require('./assets/images/Pot1.png')} />
+              <Image source={require('./assets/images/Pot2.png')} />
+              <Image source={require('./assets/images/Pot3.png')} />
+            </View>
+          </View>
+          <View style={styles.description}>
+            <Text style={{ color: 'black', fontWeight: '800', fontSize: 11 }}>DESCRIPTION</Text>
+            <Text style={{ fontSize: 12, fontWeight: '500', color: '#A9A9A9', marginTop: 6 }}>The species of the genus Astrophytum usually grow individually with spherical to columnar green shoots and reach heights of up to 1.5 meters...
+              <Text   style={{ color: 'black', fontWeight: '500', borderBottomWidth: 1, borderBottomColor: 'black',textDecorationLine:'underline' }} >detail</Text>
+            </Text>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.cart}>
+              <Image style={{ marginRight: 16 }} source={require('./assets/icons/Cart.png')} />
+              <Text style={{ color: 'white', fontWeight: '800', fontSize: 11 }}>ADD TO CART</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  area: {
+    flex: 1,
+    backgroundColor: '#F1F4FB',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  plant: {
+    marginTop: 37,
+  },
+  text: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    height: 500,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    marginHorizontal: 8,
+    paddingTop: 46,
+  },
+  selectPot: {
+    fontWeight: '800',
+    fontSize: 11,
+    color: 'black',
+    marginLeft: 30,
+    marginTop: 35,
+  },
+  pot: {
+    flexDirection: 'row',
+    marginTop: 12,
+    marginLeft: 30,
+    marginRight: 90,
+    justifyContent: 'space-between',
+  },
+  description: {
+    marginTop: 24,
+    marginHorizontal: 30,
+
+  },
+  cart: {
+    backgroundColor: "black",
+    flexDirection: 'row',
+    height: 55,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    marginHorizontal: 24,
+
+  },
+  selectButton: {
+    flexDirection:'row',
+    backgroundColor: 'white',
+    alignItems:'center',
+    justifyContent: 'space-evenly',
+    marginRight: 250,
+    marginLeft: 30,
+    marginTop: 15,
+    padding:0.1,
+    borderRadius:3,
+    borderWidth:0.2,
+    borderColor:'gray'
+  }
+
+
+})
